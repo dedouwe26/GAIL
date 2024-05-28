2a37d95942805176348b602566f9157c2175e424
dedouwe26 <63008025+dedouwe26@users.noreply.github.com>
2024-05-28 18:19:16 +0200

    tesd

---
+++ examples/packets_test/Client/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using examples.Packets.Shared;
+using GAIL.Networking;
+using GAIL.Networking.Client;
+using OxDED.Terminal;
+
+// object locker;
+// Registers all three packets.
+Packets.RegisterPackets();
+
+// Ask for port for the client to connect on.
+Terminal.Write("Port of client: ");
+string port = Terminal.ReadLine()!;
+
+// Creating a client.
+ClientContainer client = NetworkManager.CreateClient(
+    new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3003), // The server endpoint.
+    new IPEndPoint(IPAddress.Parse("127.0.0.1"), int.Parse(port)) // The local endpoint (where the client is listening).
+);
+
+// Listen to events.
+client.OnPacket+=OnPacket;
+client.OnConnect+=OnConnect;
+client.OnStop+=OnStop;
+
+Terminal.OnKeyPress += (ConsoleKey key, char keyChar, bool alt, bool shift, bool control) => {
+    client.SendPacket(new MessagePacket("test"));
+};
+
+Terminal.ListenForKeys = true;
+
+// Don't forget to start the client.
+await client.StartAsync();
+
+
+static void OnStop(ClientContainer client) {
+    // Clears whole screen
+    Terminal.WriteLine("Stopped.");
+}
+static async void OnConnect(ClientContainer client) {
+    // Send register packet.
+    Terminal.Write("Connected. Enter name: ");
+    string name = Terminal.ReadLine()!;
+    // Sends a register packet with the name.
+    await client.SendPacketAsync(new RegisterPacket(name));
+}
+static void OnPacket(ClientContainer client, Packet packet) {
+    // Check if packet is a message packet.
+    if (packet is NameMessagePacket messagePacket) {
+        // Append message to messages.
+        Terminal.WriteLine($"<{messagePacket.name}> : {messagePacket.message}");
+    }
+}
---
+++ examples/packets_test/Server/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using examples.Packets.Shared;
+using GAIL.Networking;
+using GAIL.Networking.Server;
+using OxDED.Terminal;
+
+namespace examples.Packets.Server;
+
+class Program {
+    public static void Main(string[] args) {
+        // Registers all three packets.
+        Shared.Packets.RegisterPackets();
+
+        ServerContainer server = NetworkManager.CreateServer(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3003));
+
+        // Listening to events
+        server.OnPacket+=OnPacket;
+        server.OnDisconnect+=OnDisconnect;
+
+        // Don't forget to start the server.
+        Terminal.WriteLine("Started listening...", new Style{ ForegroundColor = Color.Green});
+        server.Start();
+    }
+
+    private static void OnDisconnect(ServerContainer server, Connection connection, bool byClient, byte[] additionalData) {
+        Terminal.WriteLine(connection.GetData<string>() + " left.");
+    }
+
+    private static void OnPacket(ServerContainer server, Connection connection, Packet packet) {
+        if (packet is RegisterPacket registerPacket) {
+
+            // Checks if there already is a name set (registered).
+            if (connection.GetData<string>() == null) {
+                // Sets the data of the connection.
+                connection.SetData(registerPacket.name);
+                Terminal.WriteLine(registerPacket.name + " joined.");
+            }
+        } else if (packet is MessagePacket messagePacket) {
+            // Ensure that the client is registered.
+            if (connection.data == null) { return; }
+
+            Terminal.WriteLine($"<{connection.GetData<string>()}> : {messagePacket.message}");
+
+            // Send a NameMessagePacket back to all the clients.
+            server.BroadcastPacket(new NameMessagePacket(connection.GetData<string>()!, messagePacket.message));
+        }
+    }
+}
